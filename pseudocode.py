fad9970d6b8ca056ca45e73880c0eee10b064729
Seungyeonmoon <51092877+Seungyeonmoon@users.noreply.github.com>
2019-10-01 09:27:25 -0600

    added pseufocode.py

---
+++ pseudocode.py
@@ -0,0 +1,15 @@
+# Detective _____ (insert player's name) is invited to a dinner at a mansion.
+# 3 other people were invited to the mansion: Jay, Megan, Abriella
+# Jay is the masion owner's sister's son
+# Megan is Jay's cousin
+# Abriella is Jay and Megan's consin
+# their parents are all sibliings
+
+# It is a murder mystery
+# The host of this dinner was killed while they were having dinner
+# You inspect her body and you find out that she was poisoned
+# Suspects are Jay, Megan, Abriella
+# Host was sitting at the end of the table with you and Jay beside her,
+# Abriella was sitting next to you and Megan next to Jay
+
+# You have to search the masion for clues
