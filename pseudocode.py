a289e32519f12aefddd7d7db09c63143265815e4
Seungyeonmoon <51092877+Seungyeonmoon@users.noreply.github.com>
2019-10-02 09:30:03 -0600

    updated pseudocode.py

--- pseudocode.py
+++ pseudocode.py
@@ -1,15 +1,47 @@
 # Detective _____ (insert player's name) is invited to a dinner at a mansion.
-# 3 other people were invited to the mansion: Jay, Megan, Abriella
-# Jay is the masion owner's sister's son
+# 3 other people were invited to a masion: Jay, Megan, Abriella
+# Jay is the masion owner's nephew
 # Megan is Jay's cousin
 # Abriella is Jay and Megan's consin
 # their parents are all sibliings
 
 # It is a murder mystery
+# at the end of the game, you have to determine who the murderer is
 # The host of this dinner was killed while they were having dinner
-# You inspect her body and you find out that she was poisoned
-# Suspects are Jay, Megan, Abriella
 # Host was sitting at the end of the table with you and Jay beside her,
 # Abriella was sitting next to you and Megan next to Jay
-
+# Jay was helping the host with preparing dinner
+# Megan came a little late to the mansion and helped prepare dinner
+# I was looking around the masion with Abriella guiding me
 # You have to search the masion for clues
+# You have a notebook to keep tract of all clues
+# Suspects are Jay, Megan, Abriella
+# You inspect her body and you find out that she was poisoned
+    # With farther inspection, find a silver key in her pocket
+
+# Rooms to explore:
+    # Kitchen, host's bedroom, Jay's room, Megan's room, Abriella's room
+# In Kitchen: masion, oven, sink with many dishes
+    # masion, have to open with a bronze key
+        # in it, there is a golden key
+    # on the stove, a cattle
+    # no clues in sink, but can turn on water tab
+# In master bedroom (host's bedroom)
+    # Need silver key to go into the room
+    # Bedside table, a bronze key is found
+# Host's office
+    # Need a golden key to open
+        # In the room, a fire place, need to turn off fire
+            # pour water from sink into kettle & turn off fire
+            # In the ashes, there is a note with 4 numbers, 0117
+        # A safe that needs 4 numbers to open
+            #
+# In Jay's room
+    # a coat hanger,
+        # search the coat, find Recipt
+    # Recipt: ""
+# In Megan's room
+    # Underneath her bed
+        # Diary
+# In Abriella's room
+    #
