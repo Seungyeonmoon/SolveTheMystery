53c22bd55ccd164df4845160fe622c5a7c27db46
Seungyeonmoon <51092877+Seungyeonmoon@users.noreply.github.com>
2019-10-03 09:21:14 -0600

    updated pseudocode.py

--- pseudocode.py
+++ pseudocode.py
@@ -14,34 +14,40 @@
 # You have to search the masion for clues
 # You have a notebook to keep tract of all clues
 # Suspects are Jay, Megan, Abriella
-# You inspect her body and you find out that she was poisoned
+
+# TYPE: Insepct her body
+    # You inspect her body and you find out that she was poisoned
     # With farther inspection, find a silver key in her pocket
 
 # Rooms to explore:
     # Kitchen, host's bedroom, Jay's room, Megan's room, Abriella's room
-# TYPE: ____ (room)
-# In Kitchen: masion, oven, sink with many dishes
-    # TYPE: cabinet, oven, or sink
-    # cabinet, have to open with a bronze key
-        # in it, there is a golden key
-    # on the stove, a cattle
-    # no clues in sink, but can turn on water tab
-# In master bedroom (host's bedroom): table
+# TYPE: inspect ____ (room)
+# Kitchen: cabinet, oven, sink with many dishes
+    # TYPE: inspect _____ (cabinet, oven, or sink)
+        # cabinet, have to open with a bronze key
+            # in it, there is a golden key
+        # on the stove, a cattle
+        # no clues in sink, but can turn on water tab
+# Master bedroom (host's bedroom): bedside table
     # Need silver key to go into the room
+    # TYPE: use ____ (silver key)
+    # TYPE: inspect _____ (bedside table)
     # Bedside table, a bronze key is found
 # Host's office: fireplace, safe
     # Need a golden key to open
-        # In the room, a fire place, need to turn off fire
+    # TYPE: use ____ (golden key)
+    # TYPE: inspect ______ (fireplace, safe)
+        # fire place, need to turn off fire
             # pour water from sink into kettle & turn off fire
             # In the ashes, there is a note with 4 numbers, 0117
         # A safe that needs 4 numbers to open
             #
-# In Jay's room
-    # a coat hanger,
+# Jay's room: a coat hanger,
+    # TYPE: inspect ____ (coat hanger)
         # search the coat, find Recipt
-    # Recipt: ""
-# In Megan's room
-    # Underneath her bed
-        # Diary
-# In Abriella's room
+            # Recipt: ""
+# Megan's room: under the bed
+    # TYPE: inspect under the bed
+        # A diary is found
+# Abriella's room
     #
