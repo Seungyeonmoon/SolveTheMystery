b75054a1ff1103b561dbbbbeaf108bec68125bfd
Seungyeonmoon <51092877+Seungyeonmoon@users.noreply.github.com>
2019-10-11 08:56:27 -0600

    update pseudocode.py

--- pseudocode.py
+++ pseudocode.py
@@ -7,21 +7,31 @@
 # TYPE: insepct ____ (the host)
     # You inspect her body and you find out that she was poisoned
     # With farther inspection, find a silver key in her pocket
+        # choice: call the police, or continue inspectoion on your own
 
 # Rooms to explore:
-    # Kitchen, host's bedroom, Jay's room, Megan's room, Abriella's room
+    # Kitchen, host's bedroom, Jay's room, Megan's room, Abriella's room, back door
 # TYPE: inspect ____ (room)
-# Kitchen: cabinet, oven, sink with dishes
-    # TYPE: inspect _____ (cabinet, oven, or sink)
-        # cabinet, have to open with a bronze key
+# Kitchen: cabinet, stove, or sink, pantry
+    # TYPE: inspect _____ (cabinet, stove, pantry, or sink)
+        # cabinet, have to open with a little key
             # in it, there is a golden key
         # stove, a cattle
         # sink, but can turn on water tab
+        # pantry, a bucket on the floor
+# Back door:
+    # need little key to open the door
+        # outside, a well
+            # well has rope
+                # pull up?
+                # when pulled up, no bucket
+                 # need bucket from pantry and when pulled up, a bottle with
+                 # liquid, with inspection, it is poison
 # Master bedroom (host's bedroom): bedside table
     # Need silver key to go into the room
     # TYPE: use ____ (silver key)
     # TYPE: inspect _____ (bedside table)
-        # Bedside table, a bronze key is found
+        # Bedside table, a little key is found
 # Host's office: fireplace, safe
     # Need a golden key to open
     # TYPE: use ____ (golden key)
@@ -30,18 +40,29 @@
             # pour water from sink into kettle & turn off fire
             # In the ashes, there is a note with 4 numbers, 0117
         # A safe that needs 4 numbers to open
-            #
+            # in it, a bronze key
 # Jay's room: a coat hanger,
     # TYPE: inspect ____ (coat hanger)
         # search the coat, find Recipt
             # the recipe reads that he bought "diltiazem" medication
-            # this morning.
+            # 4 days ago.
+                # diltiazem: medication for high blood pressure, when consumed
+                # by an individual in large quantities without high blood
+                # pressure, can cause heart failure
 # Megan's room: under the bed
     # TYPE: inspect under the bed
-        # A diary is found
+        # A notebook is found
+            # Megan wrote about family members.
+            # Host: old, but healty, had 3 siblings, parents of Jay, Megan,
+                # Abriella, rich
+            # Jay: in his 30s, has a high blood pressure
+            # Megan: in her 30s, Asthma
+            # Abriella: Youngest of the cousins, in her 20s
 # Abriella's room
     # nothing to be found
 
 # The murderer is:
 # TYPE: _______ (Jay, Megan, Abriella)
     # (the murder is Jay)
+    # IF typed Megan or Abriella:
+        # She is not the murderer
