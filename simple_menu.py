626f5a064fff8b8dbab054be2f57d127bda75d9d
Seungyeonmoon <51092877+Seungyeonmoon@users.noreply.github.com>
2019-11-07 09:29:59 -0600

    update simple_menu.py

--- simple_menu.py
+++ simple_menu.py
@@ -1,11 +1,11 @@
 rooms = {
-        'Dining room': 'where the murder happened',
-        'Kitchen': {'cabinet': 'golden key',
+        'dining room': 'where the murder happened',
+        'kitchen': {'cabinet': 'golden key',
                     'stove': 'cattle',
                     'pantry': 'bucket on the floor',
                     'sink': 'turn on water tap'
                     },
-        'Back door': {'well': 'poison bottle'},
+        'back door': {'well': 'poison bottle'},
         'Lilian\'s room': {'bedside table': 'little key'},
         'Lilian\'s office': {'fireplace': 'note', 'safe': 'bronze key'},
         'Jay\'s room': {'coat hanger': 'recipt'},
@@ -22,19 +22,22 @@ def play():
     # directions & actions
     action = ['explore rooms', 'check inventory', 'quit']
     directions = ['forward', 'backward', 'left', 'right']
-    print(action)
+    print_actions(action)
     while True:
-        action_choice = player_choice("\nWhat would you like to do?")
+        action_choice = player_choice("")
         if action_choice in action:
             print(f"{action_choice.title}")
             if action_choice == 'quit':
-                sys.exit()
+                break
             elif action_choice == 'check inventory':
-                print(f"{inventory}")
+                for objects in inventory:
+                    print(f"* {objects.upper()}")
             elif action_choice == 'explore rooms':
                 choose_room()
             else:
                 print(f"{choice.title()} is not one of the choices.")
+        else:
+            print(f"{player_choice.title()} is not one of the choices.")
 
 def player_choice(text):
     """turn user input and convert it to lowercase"""
@@ -43,50 +46,20 @@ def player_choice(text):
 
 def choose_room():
     """user selects which room to go to"""
+    # print("ROOMS:")
+    # for room in rooms:
+    print(f"{room.upper()}")
+    while True:
+        input = player_choice("Which room would you like to explore?")
+        room_choice = input
+        if room_choice in rooms:
+            print(f"You are infront of {room_choice}.")
+        else:
+            print(f"{room_choice.title()} is not one of the choices.")
 
+def print_actions(action):
+    print("What would you like to do?")
+    for action_choice in action:
+        print(f"* {action_choice.upper()}")
 
-print(f"\nWhat would you like to do?")
-for action in actions:
-    print(f"\t* {action.upper()}")
-choice = input(f"")
-# when user types in check inventory
-if choice == 'check inventory':
-    for items in inventory:
-        # print list of items
-        print(f"\t*{items.upper()}")
-# when user types in explore rooms
-elif choice == 'explore rooms':
-    print("Which room would you like to explore?")
-    for room in rooms.keys():
-        # print list of rooms
-        print(f"\t* {room.upper()}")
-    room_choice = input(f"")
-    # if room choice is dining room
-    if room_choice == 'dining room':
-        print(f"The Dining room is where the murder happened.")
-    # if room choice is kitchen
-    elif room_choice == 'kitchen':
-        print("There is a pantry.")
-    # if room choice is back door
-    elif room_choice == 'back door':
-        print("The door is locked.")
-    # if room choice is lilian's office
-    elif room_choice == "lilian's office":
-        print("The door is locked.")
-    # if room choice is Lilian's room
-    elif room_choice == "lilian's room":
-        print("The door is locked.")
-    # if room choice is Megan's room
-    elif room_choice == "megan's room":
-        print("There is a notebook under the bed.")
-    # if room choice is Jay's room
-    elif room_choice == "jay's room":
-        print("There is a coat on a coat hanger")
-    # if room choice is abriella's room
-    elif room_choice == "abriella's room":
-        print(f"There is nothing in Abriella's room.")
-    else:
-        print(f"{room_choice} is not one of the options.")
-
-else:
-    print(f"{choice.title()} is not one of the choices.")
+play()
