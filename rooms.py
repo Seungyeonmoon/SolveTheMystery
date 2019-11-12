d8e2f68e38288d1c889b32bca3de0f364c27eeea
Seungyeonmoon <51092877+Seungyeonmoon@users.noreply.github.com>
2019-11-12 13:05:38 -0600

    update simple_menu.oy and upload rooms.py

---
+++ rooms.py
@@ -0,0 +1,14 @@
+rooms =[{"dining room": 'where the murder happened'},
+        {"kitchen": {'cabinet': 'golden key',
+                    'stove': 'cattle',
+                    'pantry': 'bucket on the floor',
+                    'sink': 'turn on water tap'
+                    }
+                    },
+        "back door": {'well': 'poison bottle'},
+        "Lilian's room": {'bedside table': 'little key'},
+        "Lilian's office": {'fireplace': 'note', 'safe': 'bronze key'},
+        "Jay's room": {'coat hanger': 'recipt'},
+        "Megan's room": {'under the bed': 'notebook'},
+        "Abriella's room": 'nothing to be found'
+        }
--- simple_menu.py
+++ simple_menu.py
@@ -6,9 +6,9 @@ rooms = {
                     'sink': 'turn on water tap'
                     },
         "back door": {'well': 'poison bottle'},
-        "Lilians room": {'bedside table': 'little key'},
+        "Lilian's room": {'bedside table': 'little key'},
         "Lilian's office": {'fireplace': 'note', 'safe': 'bronze key'},
-        "Jays room": {'coat hanger': 'recipt'},
+        "Jay's room": {'coat hanger': 'recipt'},
         "Megan's room": {'under the bed': 'notebook'},
         "Abriella's room": 'nothing to be found'
         }
@@ -22,25 +22,21 @@ def play():
     action = ['explore rooms', 'check inventory', 'quit']
     while True:
         print_actions(action)
-        
-        if action_choice in action:
-            if action_choice == '3':
-                break
-            elif action_choice == '2':
-                print_items(inventory)
-            elif action_choice == '1':
-                choose_room()
-            else:
-                text = choice.title()
-                print(f"Invalid choice.")
+        action_choice = int(input(""))
+        if action_choice == 3:
+            break
+        elif action_choice == 2:
+            print_items(inventory)
+        elif action_choice == 1:
+            choose_room()
         else:
             print(f"Invalid choice.")
 
 
-def player_choice(text):
-    """turn user input and convert it to lowercase"""
-    action_choice = input(text)
-    return action_choice.int()
+# def player_choice(number):
+#     """turn user input and convert it to lowercase"""
+#     action_choice = input(number)
+#     return action_choice()
 
 
 def choose_room():
@@ -58,11 +54,11 @@ def choose_room():
             print(f"{room_choice.title()} is not one of the choices.")
 
 
-# def print_actions(action):
-#     """printing actions in the right format"""
-#     print("What would you like to do?")
-#     for action_choice in action:
-#         print(f"{action.index(action_choice)+1}. {action_choice.upper()}")
+def print_actions(action):
+    """printing actions in the right format"""
+    print("What would you like to do?")
+    for action_choice in action:
+        print(f"{action.index(action_choice)+1}. {action_choice.upper()}")
 
 
 def print_rooms(rooms):
@@ -75,5 +71,6 @@ def print_items(inventory):
     """printing inventory in the right format"""
     for items in inventory:
         print(f"{inventory.index(items)+1}. {items.upper()}")
+
 
 play()
